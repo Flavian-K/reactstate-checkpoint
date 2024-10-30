b1f287d2088a4b368d406ae2b759d1ab10849e2b
Flavian-K <flaviankibe@gmail.com>
2024-10-30 20:43:37 +0300

    Add class-based component with state and lifecycle methods for profile display. Implement show/hide toggle, time interval tracking, and kawaii styling.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			person: {
+				fullName: "Flav K",
+				bio: "A software dev who is spontaneous.",
+				imgSrc: "path_to_image.jpg",
+				profession: "Software Developer Student",
+				hobbies: "traveling, dancing, going on adventures",
+				faveShowNetflix: "currently 'Hellbound' ",
+				bestPieceofTech: "Samsung Z Flip 4",
+			},
+			shows: false,
+			timeInterval: 0,
+		};
+		this.timer = null;
+	}
+
+	componentDidMount() {
+		this.timer = setInterval(() => {
+			this.setState((prevState) => ({
+				timeInterval: prevState.timeInterval + 1,
+			}));
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+	}
+
+	toggleShow = () => {
+		this.setState((prevState) => ({
+			shows: !prevState.shows,
+		}));
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<button onClick={this.toggleShow}>
+					{this.state.shows ? "Hide Profile" : "Show Profile"}
+				</button>
+
+				{this.state.shows && (
+					<div>
+						<h1>{this.state.person.fullName}</h1>
+						<p>{this.state.person.bio}</p>
+						<img src={this.state.person.imgSrc} alt="Profile" />
+						<p>{this.state.person.profession}</p>
+						<p>Hobbies: {this.state.person.hobbies}</p>
+						<p>Favorite Show on Netflix: {this.state.person.faveShowNetflix}</p>
+						<p>Best Piece of Tech: {this.state.person.bestPieceofTech}</p>
+					</div>
+				)}
+
+				<p>Time since mounted: {this.state.timeInterval} seconds</p>
+			</div>
+		);
+	}
 }
 
 export default App;
